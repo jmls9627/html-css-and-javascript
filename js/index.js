1fb5e75b6c63ab69089addcf2e6e8efc02463a1e
jmls9627 <jmls9627@gmail.com>
2019-11-09 10:34:45 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* 
+index.js
+*/
+
+
+var msg= "hello javascript";
+ alert(msg);
+console.log(msg);
+
+var resultsDiv= document.getElementById("results");
+resultsDiv.innerHTML= "<p> this is from js</p>";
+
