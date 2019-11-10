ddff6161854fc8ebfe64d599587d0451d5319801
jmls9627 <jmls9627@gmail.com>
2019-11-10 17:14:43 -0500

    new code in js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 index.js
 */
 
+"use strict"; 
 
 var msg= "hello javascript";
  alert(msg);
@@ -10,3 +11,24 @@ console.log(msg);
 var resultsDiv= document.getElementById("results");
 resultsDiv.innerHTML= "<p> this is from js</p>";
 
+console.log("msg is " + typeof(msg));
+console.log("resultDiv is" + typeof(resultsDiv));
+
+var none;
+console.log("none is " + typeof(none));
+
+var aNumber= 10;
+console.log("aNumber is " + typeof(aNumber));
+
+var truefalse= true;
+console.log("truefalse is " + typeof(truefalse));
+
+/*msgs = "this shouldn't work";*/ 
+
+if(none == undefined) {
+console.log("none is undefined "); 
+}
+
+if(aNumber == 10) {
+    console.log("10 is 10"); 
+        }
